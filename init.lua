2500d9e7dc2f904bcf3e126fab87e1279496a186
Jordan Irwin <antumdeluge@gmail.com>
2021-08-23 08:39:12 -0700

    Add some docstrings

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 
+--- Model Definitions
+--
+--  @topic models
+
+
 simple_models = {
+	--- @table node_double
+	--  @tfield string mesh node_1x2x1.obj
+	--  @tfield table box {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5}
 	node_double = {
 		mesh = "node_1x2x1.obj",
 		box = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
