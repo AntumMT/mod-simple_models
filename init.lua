febe7b2f969c8cf39f7c26f8a1ce89fcfc839f82
Jordan Irwin <antumdeluge@gmail.com>
2021-08-23 13:12:08 -0700

    Rename model "node_1x2x1" to "cube_1x2x1"

--- init.lua
+++ init.lua
@@ -4,12 +4,15 @@
 --  @topic models
 
 
+--- Global table.
+--
+--  @table simple_models
 simple_models = {
-	--- @table node_double
-	--  @tfield string mesh node_1x2x1.obj
+	--- @table simple_models.cube_1x2x1
+	--  @tfield string mesh simple_models_cube_1x2x1.obj
 	--  @tfield table box {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5}
-	node_double = {
-		mesh = "node_1x2x1.obj",
+	cube_1x2x1 = {
+		mesh = "simple_models_cube_1x2x1.obj",
 		box = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
 	},
 }
